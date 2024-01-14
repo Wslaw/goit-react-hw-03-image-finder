1faa07b32a901671e337b966b13ab5c6fac378a9
Wslaw <114644824+Wslaw@users.noreply.github.com>
2024-01-14 22:56:47 +0200

    Update Searchbar.jsx

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,4 +1,6 @@
 import { Component } from "react";
+import styles from './searchbar.module.css';
+
 
 class Searchbar extends Component {
     state = {
@@ -7,20 +9,20 @@ class Searchbar extends Component {
         per_page: 12, 
     }
 
-    
+
     render() {
         return (
-          <header class="searchbar">
-            <form class="form">
-              <button type="submit" class="button">
-                <span class="button-label">Search</span>
+          <header className={styles.searchbar}>
+            <form className={styles.form}>
+              <button type="submit" className={styles.button}>
+                <span className={styles.buttonLabel}>Search</span>
               </button>
 
               <input
-                class="input"
+                className={styles.input}
                 type="text"
-                autocomplete="off"
-                autofocus
+                autoComplete="off"
+                autoFocus
                 placeholder="Search images and photos"
               />
             </form>
